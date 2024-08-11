d0993433e89bc064624648801d4507b3793b541b
James Twose <jamestwose@Mac-mini.home>
2024-08-11 22:40:46 +0200

    getting the geoserver and postgis stuff to function as well as data upload via psql and pgadmin connections, just need to set up pygeoapi, get everything connected properly, and create a frontend that compares both things

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # geoserver-vs-pygeoapi
 Comparison of api vs tile server for rendering GIS data
+
+## Useful commands
+- `docker-compose up -d` to start the services
+- to clear ports `lsof -i tcp:5432` and `kill -9 <PID>`
+- `docker cp nyc_data.backup postgis:/var/lib/postgresql/data/nyc_data.backup` to copy the backup file to postgres container
+- `docker cp nyc_data.backup pgadmin_container:/home/nyc_data.backup` to copy the backup file to pgadmin container
+- `docker cp nyc_data.backup postgis:/home/nyc_data.backup`
+- `docker exec -it postgis psql -U postgres` to access postgis
+- `CREATE EXTENSION postgis;` to enable postgis extension
+- `pg_restore -C -d postgres /var/lib/postgresql/data/nyc_data.backup;` to restore the database
+- `pg_restore -C -d postgres /home/nyc_data.backup -U postgres` to restore the database
+- `docker exec -it pgadmin_container sh` to access pgadmin
+
+## Pgadmin connection settings
+- host: docker.for.mac.host.internal
+- port: 5434
+- user: postgres
+- password: postgres
+- database: postgres
+- schema: public
+
---
+++ pygeoapi/main.py
@@ -0,0 +1,43 @@
+import pygeoapi
+from pygeoapi.util import yaml_load
+import yaml
+import os
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+def run_api():
+    with open("pygeoapi-config-pre-update.yaml") as f:
+        config = yaml_load(f)
+    
+    config["server"]["url"] = "http://localhost:5000"
+    config["resources"]["homicides"]["providers"][0]["data"]["host"] = os.environ.get(
+        "POSTGRES_HOST"
+    )
+    config["resources"]["homicides"]["providers"][0]["data"]["dbname"] = os.environ.get(
+        "POSTGRES_DB"
+    )
+    config["resources"]["homicides"]["providers"][0]["data"]["user"] = os.environ.get(
+        "POSTGRES_USER"
+    )
+    config["resources"]["homicides"]["providers"][0]["data"]["password"] = os.environ.get(
+        "POSTGRES_PASSWORD"
+    )
+    config["resources"]["homicides"]["providers"][0]["data"]["port"] = os.environ.get(
+        "POSTGRES_PORT"
+    )
+    config["resources"]["homicides"]["providers"][0]["data"]["owner"] = os.environ.get(
+        "POSTGRES_OWNER"
+    )
+    pygeoapi.api.SwaggerUI = True
+    pygeoapi.api.config = config
+    with open("pygeoapi-config.yaml", "w") as f:
+        f.write(yaml.dump(config))
+    os.system(
+        "pygeoapi openapi generate pygeoapi-config.yaml --output-file openapi-config.yaml"
+    )
+    pygeoapi.serve()
+
+
+if __name__ == "__main__":
+    run_api()
